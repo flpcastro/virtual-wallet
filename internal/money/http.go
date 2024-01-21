08377a707692d0ee633ae5ac10f19f82c6ce7f84
Felipe <flpc.dev@gmail.com>
2024-01-21 03:28:36 -0300

    feat: create transfer handler

--- internal/money/http.go
+++ internal/money/http.go
@@ -56,3 +56,56 @@ func UsersHandler(w http.ResponseWriter, req *http.Request) {
 	w.Header().Add("Content-Type", "application/json")
 	w.Write(j)
 }
+
+func TranferHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	conn, err := database.CreateConnection()
+	if err != nil {
+		responseFromError(err, w)
+		return
+	}
+	defer conn.Close()
+
+	var tr transferRequest
+	decoder := json.NewDecoder(req.Body)
+	if err := decoder.Decode(&tr); err != nil {
+		responseFromError(err, w)
+		return
+	}
+
+	if err := validateTransfer(tr); err != nil {
+		responseFromError(err, w)
+		return
+	}
+
+	repo := PostgresRepository{Conn: conn}
+	transferService := TransferService{Repository: &repo}
+	debtorID, err := uuid.Parse(tr.DebtorID)
+	if err != nil {
+		responseFromError(err, w)
+		return
+	}
+
+	beneficiaryID, err := uuid.Parse(tr.BeneficiaryID)
+	if err != nil {
+		responseFromError(err, w)
+		return
+	}
+
+	err = transferService.Transfer(
+		tr.Amount,
+		debtorID,
+		beneficiaryID,
+	)
+
+	if err != nil {
+		responseFromError(err, w)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
