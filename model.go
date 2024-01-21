a7b369725eb7d94a809eaa9f10c75cfbecd6d874
Felipe <flpc.dev@gmail.com>
2024-01-21 02:15:59 -0300

    refactor: stop ommit balance id

--- model.go
+++ model.go
@@ -3,7 +3,7 @@ package virtualwallet
 import "github.com/google/uuid"
 
 type Balance struct {
-	ID     uuid.UUID `json:"-"`
+	ID     uuid.UUID `json:"id"`
 	Amount int       `json:"amount"`
 	UserID uuid.UUID `json:"user_id"`
 }
